7f8dce0f6587fd1477cce5a8c5ea84fc851d5f53
Jeshaiah73 <jedrac46@gmail.com>
2025-09-07 00:33:27 +0700

    Redirect After Create Post Request + Update All Components That Needed

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+
+
+
+const Footer = () => {
+
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>YourDailyTheme</strong> by <a href="/">Jeshaiah Jesse</a>. Based on the <a
+            href="/">Jazzy Blog</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
